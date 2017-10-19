670b5d8b6e8231906fb7701edc56e37e240774bf
Eric Ellingson <ellingsonster@gmail.com>
2017-10-18 20:10:42 -0700

    added staging config

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const path = require('path');
 const express = require('express');
 
 const config = require('./config');
+const routes = require('./routes');
 
 const app = express();
 
 app.use('/static', express.static(path.join(__dirname, './static')));
+
+app.use('/song-q', routes.songq);
+app.use('/api', routes.api);
 
 app.use('/', (req, res) =>
   res.sendFile(path.join(__dirname, './views/index.html'))
